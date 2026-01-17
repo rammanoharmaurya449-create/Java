1422a3fb7c730a19d90712cc7950fd275ca66d6c
Ram Manohar Maurya <rammanoharmaurya870@gmail.com>
2026-01-18 01:46:03 +0530

    Added StudentDetails program

---
+++ StudentDetails.java
@@ -0,0 +1,14 @@
+public class StudentDetails{
+    public static void main(String args[]){
+        String name="Ram Manohar Maurya";
+        int prn=151;
+        String address="Pune, Maharashtra";
+        long mobile = +917822929451L;
+
+        System.out.println("Name : "+ name);
+        System.out.println("PRN : " +prn);
+        System.out.println("Address : "+ address);
+        System.out.println("Mobile Number : "+ mobile);
+
+    }
+}
