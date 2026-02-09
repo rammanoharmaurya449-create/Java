e41e4efa56533b51ade5054a31cad38fc6463eb7
Ram Manohar Maurya <rammanoharmaurya870@gmail.com>
2026-02-09 09:01:10 +0530

    Day 7

---
+++ Function/Array.java
@@ -0,0 +1,17 @@
+public class Array {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
---
+++ Function/ArrayListExample.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+
+    
+        ArrayList<String> fruits = new ArrayList<>();
+
+        
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+
+        
+        System.out.println("Fruits: " + fruits);
+
+        
+        fruits.remove("Banana");
+        System.out.println("After removing Banana: " + fruits);
+
+        
+        System.out.println("Size: " + fruits.size());
+
+    
+        System.out.println("Contains Apple " + fruits.contains("Apple"));
+    }
+}
